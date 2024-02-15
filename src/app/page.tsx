816c98fd0261373fddce538f9fff3807b3597e22
karthikshetty <karhtikshetty1@gmail.com>
2024-02-15 21:49:14 +0530

    almost done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import ECommerce from "@/components/Dashboard/E-commerce";
-import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import { auth } from '../config/firebase'
 import { onAuthStateChanged } from "firebase/auth";
