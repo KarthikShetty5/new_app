50356ae414593ed4bab94a8585e88561ee5cdacb
karthikshetty <karhtikshetty1@gmail.com>
2024-02-15 19:17:51 +0530

    not done

--- src/app/chart/page.tsx
+++ src/app/chart/page.tsx
@@ -3,16 +3,20 @@ import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import React from "react";
 
+interface ChartOneProps {
+  prop: string;
+}
+
 export const metadata: Metadata = {
   title: "Next.js Chart | TailAdmin - Next.js Dashboard Template",
   description:
     "This is Next.js Chart page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
 };
 
-const BasicChartPage: React.FC = () => {
+const BasicChartPage: React.FC<ChartOneProps> = () => {
   return (
     <DefaultLayout>
-      <Chart />
+      <Chart prop={''} />
     </DefaultLayout>
   );
 };
