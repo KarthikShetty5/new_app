eb376cdd4ea873bcfb40a726877ed84cef20bed1
karthikshetty <karhtikshetty1@gmail.com>
2024-02-18 20:36:37 +0530

    some changes

--- src/components/FormElements/index.tsx
+++ src/components/FormElements/index.tsx
@@ -1,43 +1,26 @@
-// @ts-nocheck
 "use client";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-import CheckboxFive from "@/components/Checkboxes/CheckboxFive";
-import CheckboxFour from "@/components/Checkboxes/CheckboxFour";
-import CheckboxOne from "@/components/Checkboxes/CheckboxOne";
-import CheckboxThree from "@/components/Checkboxes/CheckboxThree";
-import CheckboxTwo from "@/components/Checkboxes/CheckboxTwo";
-import SwitcherFour from "@/components/Switchers/SwitcherFour";
-import SwitcherOne from "@/components/Switchers/SwitcherOne";
-import SwitcherThree from "@/components/Switchers/SwitcherThree";
-import SwitcherTwo from "@/components/Switchers/SwitcherTwo";
-import DatePickerTwo from "@/components/FormElements/DatePicker/DatePickerTwo";
-import DatePickerOne from "@/components/FormElements/DatePicker/DatePickerOne";
-import MultiSelect from "@/components/FormElements/MultiSelect";
-import SelectGroupTwo from "@/components/SelectGroup/SelectGroupTwo";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
 import { useState } from "react";
-import 'firebase/storage';
-import 'firebase/firestore';
-import { firebase } from 'firebase/app';
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 import '../../config/firebase'
 import { useRouter } from 'next/navigation'
 
-const FormElements = () => {
-  const [file, setFile]: any = useState(null);
+const FormElements: React.FC = () => {
+  const [file, setFile] = useState<File | null>(null);
   const router = useRouter()
 
-  const handleFileChange = (e: any) => {
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
 
-    if (e.target.files[0]) {
+    if (e.target.files && e.target.files[0]) {
       setFile(e.target.files[0]);
     }
   };
 
-  const handleUpload = async (e: any) => {
+  const handleUpload = async (e: React.FormEvent) => {
     e.preventDefault();
     if (!file) {
       toast.error('No File Selected !', { //this is react-toastify which is used to show up notification
@@ -51,13 +34,13 @@ const FormElements = () => {
       });
       return;
     }
-    const storage = getStorage(firebase);
+    const storage = getStorage();
     const storageRef = ref(storage, `uploads/${file.name}`);
 
     try {
       await uploadBytes(storageRef, file);
       const fileUrl = await getDownloadURL(storageRef);
-      const firestore = getFirestore(firebase);
+      const firestore = getFirestore();
       const filesCollection = collection(firestore, 'files');
       toast.success('File uploaded successfully!', {
         position: "top-left",
