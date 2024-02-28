e97de93616ad8672ee5d3524496104dcee8daf1d
karthikshetty <karhtikshetty1@gmail.com>
2024-02-28 23:03:50 +0530

    seperated backend from frontend

---
+++ src/app/api/recent.js
@@ -0,0 +1,7 @@
+
+export default async function handler(req, res) {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/data/recent`);
+    const data = await response.json();
+    res.status(200).json(data);
+}
+
